737daf3257daabe84f7d0149f6624c5d523d6a46
apachertr180navneeet <navneet@brenin.co>
2026-07-26 02:31:57 +0530

    feat: implement document management views for administration dashboard

--- resources/views/admin/documents/index.blade.php
+++ resources/views/admin/documents/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<div class="container-xxl flex-grow-1 container-p-y">
+<div class="container-fluid flex-grow-1 container-p-y">
 
     <div class="d-flex justify-content-between align-items-center mb-4">
         <div>
